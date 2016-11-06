bd283082bd78861ff090a3a968f6cd9122416367
tychota <tychota@users.noreply.github.com>
2016-11-06 18:52:58 +0100

    Add exercice 1 solution

---
+++ exercice-01-solution/screens/HomeScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import { BaseText } from 'learnapollo/components/BaseText';
+
+export default class HomeScreen extends React.Component {
+  static route = {
+    navigationBar: {
+      visible: false,
+    },
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            <BaseText
+            fontFace="lato-bold"
+            style={{fontSize: 22}}
+          >
+            Pokedex
+          </BaseText>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+});
