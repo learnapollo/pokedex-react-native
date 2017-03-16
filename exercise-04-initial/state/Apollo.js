9215aede2b48cee669a00e7e842c7c92c14bdcb5
Johannes Schickling <schickling.j@gmail.com>
2017-03-16 11:30:37 +0100

    cleanup

--- exercise-04-initial/state/Apollo.js
+++
@@ -1,9 +0,0 @@
-import ApolloClient, { createNetworkInterface } from 'apollo-client';
-
-const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: 'https://api.graph.cool/simple/v1/__PROJECT_ID__'
-  }),
-});
-
-export default client;
